877a9d522b7f82d618770391a749216f192fd663
Christian Sutter <git@doublesignal.com>
2023-08-25 13:55:36 +0000

    Add, set up, and configure Rubocop
    - Add the `rubocop-govuk` gem
    - Configure it according to Developer Docs conventions
    - Run an initial autocorrect

--- Gemfile
+++ Gemfile
@@ -3,11 +3,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.2"
 
-RAILS_GEMS_VERSION = "7.0.7.2"
-gem "railties", RAILS_GEMS_VERSION
+RAILS_GEMS_VERSION = "7.0.7.2".freeze
 gem "actionpack", RAILS_GEMS_VERSION
 gem "activemodel", RAILS_GEMS_VERSION
 gem "activesupport", RAILS_GEMS_VERSION
+gem "railties", RAILS_GEMS_VERSION
 
 gem "puma", "~> 5.0"
 
@@ -16,7 +16,8 @@ group :test do
 end
 
 group :development, :test do
-  gem "debug", platforms: %i[ mri mingw x64_mingw ]
+  gem "debug", platforms: %i[mri mingw x64_mingw]
   gem "govuk_test"
   gem "rspec-rails"
+  gem "rubocop-govuk", require: false
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -38,7 +38,6 @@ Rails.application.configure do
 
   # Tell Active Support which deprecation messages to disallow.
   config.active_support.disallowed_deprecation_warnings = []
-
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
