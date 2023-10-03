d83f1ecf01e92470704f98979b6645a3fae1e355
Christian Sutter <git@doublesignal.com>
2023-10-03 15:01:37 +0000

    Extract content from message hash
    Adds `jsonpath` gem and uses it to extract all indexing-relevant content
    from an incoming document message into a single string to treat as
    unstructured content for the search engine.

--- spec/lib/publishing_event_pipeline/extractors/content_spec.rb
+++ spec/lib/publishing_event_pipeline/extractors/content_spec.rb
@@ -2,26 +2,94 @@ RSpec.describe PublishingEventPipeline::Extractors::Content do
   describe "#call" do
     subject(:content) { described_class.new.call(message_hash) }
 
-    context "when body is present" do
+    describe "with basic top-level fields" do
       let(:message_hash) do
         {
           "details" => {
-            "body" => "Lorem ipsum dolor sit amet.",
+            "body" => "a",
+            "description" => "b",
+            "hidden_search_terms" => "c",
+            "introduction" => "d",
+            "introductory_paragraph" => "e",
+            "more_information" => "f",
+            "need_to_know" => "g",
+            "summary" => "h",
+            "title" => "i",
           },
         }
       end
 
-      it { is_expected.to eq("Lorem ipsum dolor sit amet.") }
+      it { is_expected.to eq("a\nb\nc\nd\ne\nf\ng\nh\ni") }
     end
 
-    context "when body is not present" do
+    describe "with hidden indexable content" do
+      let(:message_hash) do
+        {
+          "details" => {
+            "metadata" => {
+              "hidden_indexable_content" => %w[x y z],
+            },
+          },
+        }
+      end
+
+      it { is_expected.to eq("x\ny\nz") }
+    end
+
+    describe "with a project code" do
+      let(:message_hash) do
+        {
+          "details" => {
+            "metadata" => {
+              "project_code" => "PRINCE2",
+            },
+          },
+        }
+      end
+
+      it { is_expected.to eq("PRINCE2") }
+    end
+
+    describe "with contact groups" do
+      let(:message_hash) do
+        {
+          "details" => {
+            "contact_groups" => [
+              { "title" => "x" },
+              { "title" => "y" },
+              { "title" => "z" },
+            ],
+          },
+        }
+      end
+
+      it { is_expected.to eq("x\ny\nz") }
+    end
+
+    describe "with parts" do
+      let(:message_hash) do
+        {
+          "details" => {
+            "parts" => [
+              { "title" => "x", "body" => "a" },
+              { "title" => "y", "body" => "b" },
+              { "title" => "z", "body" => "c" },
+            ],
+          },
+        }
+      end
+
+      it { is_expected.to eq("x\ny\nz\na\nb\nc") }
+    end
+
+    describe "without any fields" do
       let(:message_hash) do
         {
           "details" => {},
         }
       end
 
-      it { is_expected.to be_nil }
+      it { is_expected.to be_blank }
     end
   end
 end
