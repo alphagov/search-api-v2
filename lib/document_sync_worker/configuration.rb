fee0180df7f977a7b7d9657d0a44325382a506f6
Christian Sutter <git@doublesignal.com>
2023-10-09 14:01:37 +0000

    Allow worker to depend on Rails environment
    We've decided that we won't extract the document sync worker from the
    Rails application for the sake of convenience and avoiding having
    several repos with only slightly different dependencies and
    configuration.
    
    - Remove custom logger from `DocumentSyncWorker` and just use Rails
      logger instead
    - Allow Rake task to depend on Rails environment to make sure
      `govuk_app_config` is properly initialised throughout

--- lib/document_sync_worker/configuration.rb
+++ lib/document_sync_worker/configuration.rb
@@ -3,18 +3,7 @@ module DocumentSyncWorker
     attr_accessor :logger, :message_queue_name, :repository
 
     def initialize
-      @logger = ActiveSupport::Logger.new($stdout, progname: "DocumentSyncWorker")
-      @logger.formatter = proc do |level, datetime, progname, message|
-        hash = {
-          message:,
-          level:,
-          progname:,
-          "@timestamp": datetime.utc.iso8601(3),
-          tags: %w[document_sync_worker],
-        }
-
-        "#{hash.to_json}\n"
-      end
+      @logger = Rails.logger
     end
   end
 end
