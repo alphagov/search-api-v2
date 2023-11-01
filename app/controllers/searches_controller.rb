2d0f5f66f11ef3f3a6972c6a532bd31491098f7d
Christian Sutter <git@doublesignal.com>
2023-11-01 08:09:37 +0000

    Move from `ReadRepository` to a service object
    Having tried on a few patterns for size and found none of them a great
    fit (either too much or too little abstraction), this seems like a
    sensible middle ground that allows for reasonably easy and not too hacky
    testing and is idiomatic enough to be understood by someone coming in to
    look at this application in the future.
    
    - Add `DiscoveryEngine::Search` service to abstract communication with
      the Discovery Engine client and return a `ResultSet`
    - Get results from this service object in `SearchesController`
    - Stub instantiation of the service in the search request spec
    - Remove now unneeded `Query` and `ReadRepository` classes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,17 @@
 class SearchesController < ApplicationController
   def show
-    render json: Query.new.result_set
+    result_set = DiscoveryEngine::Search.new.call(
+      query_params[:q],
+      start: query_params[:start]&.to_i,
+      count: query_params[:count]&.to_i,
+    )
+
+    render json: result_set
+  end
+
+private
+
+  def query_params
+    params.permit(:q, :start, :count)
   end
 end
--- spec/requests/search_request_spec.rb
+++ spec/requests/search_request_spec.rb
@@ -1,14 +1,27 @@
 RSpec.describe "Making a search request" do
+  let(:search_service) { instance_double(DiscoveryEngine::Search, call: result_set) }
+  let(:result_set) { ResultSet.new(results: [], total: 42, start: 21) }
+
+  before do
+    allow(DiscoveryEngine::Search).to receive(:new).and_return(search_service)
+  end
+
   describe "GET /search.json" do
-    it "successfully returns a minimally acceptable zero results response for finder-frontend" do
+    it "returns the result set as JSON" do
       get "/search.json"
 
       expect(response).to have_http_status(:ok)
       expect(JSON.parse(response.body)).to eq({
         "results" => [],
-        "total" => 0,
-        "start" => 0,
+        "total" => 42,
+        "start" => 21,
       })
+    end
+
+    it "passes any query parameters to the search service in the expected format" do
+      get "/search.json?q=garden+centres&start=11&count=22"
+
+      expect(search_service).to have_received(:call).with("garden centres", start: 11, count: 22)
     end
   end
 end
