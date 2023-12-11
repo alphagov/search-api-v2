7a3c5f06e9ae4ee12db7ca236ac07143b9a93dbb
Christian Sutter <git@doublesignal.com>
2023-12-11 10:58:23 +0000

    Make filters more generic
    Any filterable field should be able to be filtered as a reject/any/all
    filter.

--- spec/services/discovery_engine/query/filters_spec.rb
+++ spec/services/discovery_engine/query/filters_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe DiscoveryEngine::Query::Filters do
       it { is_expected.to be_nil }
     end
 
-    context "with reject_link" do
+    context "with a reject filter" do
       context "with an empty parameter" do
         let(:query_params) { { q: "garden centres", reject_link: "" } }
 
@@ -28,7 +28,7 @@ RSpec.describe DiscoveryEngine::Query::Filters do
       end
     end
 
-    context "with filter_content_purpose_supergroup" do
+    context "with an 'any' filter" do
       context "with an empty parameter" do
         let(:query_params) { { q: "garden centres", filter_content_purpose_supergroup: "" } }
 
@@ -36,22 +36,55 @@ RSpec.describe DiscoveryEngine::Query::Filters do
       end
 
       context "with a single parameter" do
-        let(:query_params) { { q: "garden centres", filter_content_purpose_supergroup: "services" } }
+        let(:query_params) do
+          { q: "garden centres", filter_content_purpose_supergroup: "services" }
+        end
 
         it { is_expected.to eq('(content_purpose_supergroup: ANY("services"))') }
       end
 
       context "with several parameters" do
-        let(:query_params) { { q: "garden centres", filter_content_purpose_supergroup: %w[services guidance] } }
+        let(:query_params) do
+          { q: "garden centres", filter_content_purpose_supergroup: %w[services guidance] }
+        end
 
         it { is_expected.to eq('(content_purpose_supergroup: ANY("services","guidance"))') }
       end
     end
 
-    context "with several filters specified" do
-      let(:query_params) { { q: "garden centres", reject_link: "/foo", filter_content_purpose_supergroup: "services" } }
+    context "with an 'all' filter" do
+      context "with an empty parameter" do
+        let(:query_params) { { q: "garden centres", filter_all_part_of_taxonomy_tree: "" } }
 
-      it { is_expected.to eq('(NOT link: ANY("/foo")) AND (content_purpose_supergroup: ANY("services"))') }
+        it { is_expected.to be_nil }
+      end
+
+      context "with a single parameter" do
+        let(:query_params) { { q: "garden centres", filter_all_part_of_taxonomy_tree: "cafe-1234" } }
+
+        it { is_expected.to eq('(part_of_taxonomy_tree: ANY("cafe-1234"))') }
+      end
+
+      context "with several parameters" do
+        let(:query_params) do
+          { q: "garden centres", filter_all_part_of_taxonomy_tree: %w[cafe-1234 face-5678] }
+        end
+
+        it { is_expected.to eq('(part_of_taxonomy_tree: ANY("cafe-1234") AND part_of_taxonomy_tree: ANY("face-5678"))') }
+      end
+    end
+
+    context "with several filters specified" do
+      let(:query_params) do
+        {
+          q: "garden centres",
+          reject_link: "/foo",
+          filter_content_purpose_supergroup: "services",
+          filter_all_part_of_taxonomy_tree: %w[cafe-1234 face-5678],
+        }
+      end
+
+      it { is_expected.to eq('(NOT link: ANY("/foo")) AND (content_purpose_supergroup: ANY("services")) AND (part_of_taxonomy_tree: ANY("cafe-1234") AND part_of_taxonomy_tree: ANY("face-5678"))') }
     end
 
     context "with filters containing escapable characters" do
