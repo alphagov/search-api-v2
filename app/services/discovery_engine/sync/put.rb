98dd6e82a34893989ccc06bd457720821eaaf04f
Christian Sutter <git@doublesignal.com>
2024-04-03 13:53:35 +0000

    Amend add/update sync flow to use locking
    - Acquire lock before trying to add/update a document
    - Fail and do not add/update document if lock is not able to be acquired
    - Do not add/update a document if the remote version is newer

--- app/services/discovery_engine/sync/put.rb
+++ app/services/discovery_engine/sync/put.rb
@@ -3,6 +3,7 @@ module DiscoveryEngine::Sync
     MIME_TYPE = "text/html".freeze
 
     include DocumentName
+    include Locking
     include Logging
 
     def initialize(client: ::Google::Cloud::DiscoveryEngine.document_service(version: :v1))
@@ -10,19 +11,35 @@ module DiscoveryEngine::Sync
     end
 
     def call(content_id, metadata, content: "", payload_version: nil)
-      client.update_document(
-        document: {
-          id: content_id,
-          name: document_name(content_id),
-          json_data: metadata.merge(payload_version:).to_json,
-          content: {
-            mime_type: MIME_TYPE,
-            # The Google client expects an IO object to extract raw byte content from
-            raw_bytes: StringIO.new(content),
+      with_locked_document(content_id, payload_version:) do
+        if payload_newer_than_remote?(content_id, payload_version:)
+          log(
+            Logger::Severity::INFO,
+            "Ignored as newer version (#{latest_synced_version(content_id)}) already synced",
+            content_id:, payload_version:,
+          )
+          Metrics::Exported.increment_counter(
+            :discovery_engine_requests, type: "put", status: "ignored_outdated"
+          )
+          return
+        end
+
+        client.update_document(
+          document: {
+            id: content_id,
+            name: document_name(content_id),
+            json_data: metadata.merge(payload_version:).to_json,
+            content: {
+              mime_type: MIME_TYPE,
+              # The Google client expects an IO object to extract raw byte content from
+              raw_bytes: StringIO.new(content),
+            },
           },
-        },
-        allow_missing: true,
-      )
+          allow_missing: true,
+        )
+
+        set_latest_synced_version(content_id, payload_version)
+      end
 
       log(Logger::Severity::INFO, "Successfully added/updated", content_id:, payload_version:)
       Metrics::Exported.increment_counter(
