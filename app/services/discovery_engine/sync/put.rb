0a506ae7dd0ef6269fc6fc7d95ba59162388cae7
Christian Sutter <git@doublesignal.com>
2024-05-31 11:23:17 +0000

    Update `Put` to use `DocumentLock`

--- app/services/discovery_engine/sync/put.rb
+++ app/services/discovery_engine/sync/put.rb
@@ -10,34 +10,34 @@ module DiscoveryEngine::Sync
     end
 
     def call
-      with_locked_document do
-        if outdated_payload_version?
-          log(
-            Logger::Severity::INFO,
-            "Ignored as newer version (#{latest_synced_version}) already synced",
-          )
-          Metrics::Exported.increment_counter(
-            :discovery_engine_requests, type: "put", status: "ignored_outdated"
-          )
-          return
-        end
+      lock.acquire
 
-        client.update_document(
-          document: {
-            id: content_id,
-            name: document_name,
-            json_data: metadata.merge(payload_version:).to_json,
-            content: {
-              mime_type: MIME_TYPE,
-              # The Google client expects an IO object to extract raw byte content from
-              raw_bytes: StringIO.new(content),
-            },
-          },
-          allow_missing: true,
+      if outdated_payload_version?
+        log(
+          Logger::Severity::INFO,
+          "Ignored as newer version (#{latest_synced_version}) already synced",
         )
-
-        set_latest_synced_version
+        Metrics::Exported.increment_counter(
+          :discovery_engine_requests, type: "put", status: "ignored_outdated"
+        )
+        return
       end
+
+      client.update_document(
+        document: {
+          id: content_id,
+          name: document_name,
+          json_data: metadata.merge(payload_version:).to_json,
+          content: {
+            mime_type: MIME_TYPE,
+            # The Google client expects an IO object to extract raw byte content from
+            raw_bytes: StringIO.new(content),
+          },
+        },
+        allow_missing: true,
+      )
+
+      set_latest_synced_version
 
       log(Logger::Severity::INFO, "Successfully added/updated")
       Metrics::Exported.increment_counter(
@@ -50,6 +50,8 @@ module DiscoveryEngine::Sync
       )
       GovukError.notify(e)
       Metrics::Exported.increment_counter(:discovery_engine_requests, type: "put", status: "error")
+    ensure
+      lock.release
     end
 
   private
