2d47b697dd34fe155a5dd0ff06bce838d04aac19
Christian Sutter <git@doublesignal.com>
2024-06-05 16:06:16 +0000

    Implement `DocumentVersionCache#outdated?`
    This copies over the logic from the `DiscoveryEngine::Sync::Versioning`
    module, adding some tidier specs.

--- app/services/coordination/document_version_cache.rb
+++ app/services/coordination/document_version_cache.rb
@@ -10,8 +10,27 @@ module Coordination
       @payload_version = payload_version
     end
 
+    # Checks whether this document version is outdated (because the cache tracks a newer version).
+    def outdated?
+      # Sense check: This shouldn't ever come through as nil from Publishing API, but if it does,
+      # the only really useful thing we can do is ignore this check entirely because we can't
+      # meaningfully make a comparison.
+      return false if payload_version.nil?
+
+      # If there is no remote version yet, our version is always newer by definition
+      return false if latest_synced_version.nil?
+
+      latest_synced_version.to_i >= payload_version.to_i
+    end
+
   private
 
     attr_reader :content_id, :payload_version
+
+    def latest_synced_version
+      Rails.application.config.redis_pool.with do |redis|
+        redis.get("#{VERSION_KEY_PREFIX}:#{content_id}")&.to_i
+      end
+    end
   end
 end
