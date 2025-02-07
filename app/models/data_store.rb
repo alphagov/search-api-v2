b3c48a2d1cf9ebc6eb0f612c74f268646a302bf5
Christian Sutter <git@doublesignal.com>
2025-02-07 10:33:07 +0000

    Copy read-only Discovery Engine models from Search Admin
    In the vein of "a little duplication is better than the wrong
    abstraction", we probably want to avoid having a shared library for the
    handful of relatively trivial shared data models between this app and
    Search Admin.
    
    - Copy `DataStore` and `Engine` models (and `DiscoveryEngineNameable`
      concern) from Search Admin into this app

---
+++ app/models/data_store.rb
@@ -0,0 +1,15 @@
+# Represents a data store on Discovery Engine.
+#
+# A data store contains the indexed documents that can be searched through an engine.
+#
+# Our architecture currently only has a single data store, so we do not need the ability to manage
+# data stores through Search Admin.
+#
+# see https://cloud.google.com/ruby/docs/reference/google-cloud-discovery_engine-v1/latest/Google-Cloud-DiscoveryEngine-V1-DataStore
+DataStore = Data.define(:remote_resource_id) do
+  include DiscoveryEngineNameable
+
+  def self.default
+    new("govuk_content")
+  end
+end
---
+++ app/models/engine.rb
@@ -0,0 +1,18 @@
+# Represents an engine on Discovery Engine.
+#
+# An engine (called "app" in the Google Cloud UI) is an abstraction over the data stores that
+# contain our searchable documents, and is used for querying. It is the parent resource of several
+# other resources such as controls and serving configs.
+#
+# Our architecture currently only has a single engine, so we do not need the ability to manage
+# engines through Search Admin.
+#
+# see https://cloud.google.com/ruby/docs/reference/google-cloud-discovery_engine-v1/latest/Google-Cloud-DiscoveryEngine-V1-Engine
+Engine = Data.define(:remote_resource_id) do
+  include DiscoveryEngineNameable
+
+  # The default engine created through Terraform in `govuk-infrastructure`
+  def self.default
+    new("govuk")
+  end
+end
