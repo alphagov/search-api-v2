1955afed6f0973fd24de43191f695cbeba80679b
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2025-06-11 14:23:47 +0100

    Import data to empty set

--- app/services/discovery_engine/evaluation/sample_query_set.rb
+++ app/services/discovery_engine/evaluation/sample_query_set.rb
@@ -1,11 +1,16 @@
 module DiscoveryEngine
   module Evaluation
     class SampleQuerySet
+      BIGQUERY_DATASET_ID = "automated_evaluation_input".freeze
+      BIGQUERY_TABLE_ID = "clickstream".freeze
+
       def initialize(month, year, set)
         @month = month
         @year = year
         @set = set
       end
+
+      attr_reader :set
 
       def self.create(month:, year:)
         sample_query_set = DiscoveryEngine::Clients
@@ -19,6 +24,30 @@ module DiscoveryEngine
             parent: Rails.application.config.discovery_engine_default_location_name,
           )
         new(month, year, sample_query_set)
+      end
+
+      def import_from_bigquery
+        operation = DiscoveryEngine::Clients
+          .sample_query_service
+          .import_sample_queries(
+            parent: set.name,
+            bigquery_source: {
+              dataset_id: BIGQUERY_DATASET_ID,
+              table_id: BIGQUERY_TABLE_ID,
+              project_id: Rails.application.config.google_cloud_project_id,
+              partition_date: {
+                year: year,
+                month: month,
+                # Partition date needs to be a full date not just year-month
+                day: 1,
+              },
+            },
+          )
+        operation.wait_until_done!
+
+        raise operation.error.message if operation.error?
+
+        Rails.logger.info("Successfully imported sample queries into: #{set.name}")
       end
 
     private
--- spec/services/discovery_engine/evaluation/sample_query_set_spec.rb
+++ spec/services/discovery_engine/evaluation/sample_query_set_spec.rb
@@ -3,10 +3,13 @@ RSpec.describe DiscoveryEngine::Evaluation::SampleQuerySet do
 
   let(:month) { "1" }
   let(:year) { "2025" }
-  let(:sample_query_set_service_stub) { double("sample_query_set_service", create_sample_query_set: true) }
+  let(:operation_object) { double("operation", wait_until_done!: true, error?: false) }
+  let(:response_object) { double("response", name: "name") }
+  let(:sample_query_set_service_stub) { double("sample_query_set_service", create_sample_query_set: response_object) }
+  let(:sample_query_service_stub) { double("sample_query_service", import_sample_queries: operation_object) }
 
   before do
-    allow(DiscoveryEngine::Clients).to receive(:sample_query_set_service).and_return(sample_query_set_service_stub)
+    allow(DiscoveryEngine::Clients).to receive_messages(sample_query_set_service: sample_query_set_service_stub, sample_query_service: sample_query_service_stub)
   end
 
   describe ".create" do
@@ -21,6 +24,42 @@ RSpec.describe DiscoveryEngine::Evaluation::SampleQuerySet do
         sample_query_set_id: "clickstream_#{year}-#{month}",
         parent: Rails.application.config.discovery_engine_default_location_name,
       )
+    end
+  end
+
+  describe "#import_from_bigquery" do
+    let(:bigquery_source) do
+      {
+        dataset_id: described_class::BIGQUERY_DATASET_ID,
+        table_id: described_class::BIGQUERY_TABLE_ID,
+        project_id: Rails.application.config.google_cloud_project_id,
+        partition_date: {
+          year: year,
+          month: month,
+          day: 1,
+        },
+      }
+    end
+
+    it "calls the import_sample_queries endpoint on the sample query service client" do
+      sqs = sample_query_set.create(month:, year:)
+      sqs.import_from_bigquery
+
+      expect(sample_query_service_stub).to have_received(:import_sample_queries).with(
+        parent: sqs.set.name,
+        bigquery_source: bigquery_source,
+      )
+    end
+
+    context "when operation does not complete" do
+      let(:error_stub) { double("error", message: "An error message") }
+      let(:operation_object) { double("operation", wait_until_done!: true, error?: true, error: error_stub) }
+
+      it "raises an error" do
+        sqs = sample_query_set.create(month:, year:)
+
+        expect { sqs.import_from_bigquery }.to raise_error("An error message")
+      end
     end
   end
 end
