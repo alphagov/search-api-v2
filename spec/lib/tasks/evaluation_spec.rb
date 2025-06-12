37e8a7e5afa1eac224b52d097da69719ef4653a4
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2025-06-12 18:32:40 +0100

    fetch_evaluations rake task

--- spec/lib/tasks/evaluation_spec.rb
+++ spec/lib/tasks/evaluation_spec.rb
@@ -1,9 +1,10 @@
 RSpec.describe "Evaluation tasks" do
-  describe "evaluation:clickstream:setup_sample_set" do
+  describe "setup_sample_set" do
     let(:sample_query_set) { instance_double(DiscoveryEngine::Evaluation::SampleQuerySet) }
 
     before do
       Rake::Task["evaluation:clickstream:setup_sample_set"].reenable
+
       allow(DiscoveryEngine::Evaluation::SampleQuerySet)
       .to receive(:new)
       .and_return(sample_query_set)
@@ -14,6 +15,33 @@ RSpec.describe "Evaluation tasks" do
         .to receive(:create_and_import)
         .once
       Rake::Task["evaluation:clickstream:setup_sample_set"].invoke
+    end
+  end
+
+  describe "fetch_evaluations" do
+    let(:evaluation_resource) { instance_double(DiscoveryEngine::Evaluation::EvaluationResource) }
+
+    before do
+      Rake::Task["evaluation:clickstream:fetch_evaluations"].reenable
+
+      allow(DiscoveryEngine::Evaluation::EvaluationResource)
+        .to receive(:new)
+        .with("clickstream_01_07")
+        .and_return(evaluation_resource)
+    end
+
+    it "creates and outputs evaluations" do
+      expect(evaluation_resource)
+        .to receive(:fetch_quality_metrics)
+        .once
+      Rake::Task["evaluation:clickstream:fetch_evaluations"].invoke("clickstream_01_07")
+    end
+
+    context "when sample_id is not passed in" do
+      it "raises and error" do
+        expect { Rake::Task["evaluation:clickstream:fetch_evaluations"].invoke }
+          .to raise_error("sample_set_id is required")
+      end
     end
   end
 end
