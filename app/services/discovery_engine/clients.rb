81b6f634224d841c3020bea4b07ae87b053ecce4
Christian Sutter <git@doublesignal.com>
2025-06-16 11:01:50 +0100

    Add additional services to `DiscoveryEngine::Clients`

--- app/services/discovery_engine/clients.rb
+++ app/services/discovery_engine/clients.rb
@@ -4,16 +4,32 @@ module DiscoveryEngine
   module Clients
     extend self
 
-    def sample_query_set_service
-      @sample_query_set_service ||= v1beta_api::SampleQuerySetService::Client.new
+    def completion_service
+      @completion_service ||= Google::Cloud::DiscoveryEngine.completion_service(version: :v1)
+    end
+
+    def document_service
+      @document_service ||= Google::Cloud::DiscoveryEngine.document_service(version: :v1)
+    end
+
+    def evaluation_service
+      @evaluation_service ||= v1beta_api::EvaluationService::Client.new
     end
 
     def sample_query_service
       @sample_query_service ||= v1beta_api::SampleQueryService::Client.new
     end
 
-    def evaluation_service
-      @evaluation_service ||= v1beta_api::EvaluationService::Client.new
+    def sample_query_set_service
+      @sample_query_set_service ||= v1beta_api::SampleQuerySetService::Client.new
+    end
+
+    def search_service
+      @search_service ||= Google::Cloud::DiscoveryEngine.search_service(version: :v1)
+    end
+
+    def user_event_service
+      @user_event_service ||= Google::Cloud::DiscoveryEngine.user_event_service(version: :v1)
     end
 
   private
