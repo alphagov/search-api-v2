24800fb295a379e072a772d05a25aab22ea4e9ac
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2025-07-01 21:42:16 +0100

    Create SampleQuerySetFields module

---
+++ app/services/discovery_engine/quality/sample_query_set_fields.rb
@@ -0,0 +1,22 @@
+module DiscoveryEngine
+  module Quality::SampleQuerySetFields
+    BIGQUERY_DATASET_ID = "automated_evaluation_input".freeze
+    BIGQUERY_TABLE_ID = "clickstream".freeze
+
+    def display_name(date)
+      "#{BIGQUERY_TABLE_ID} #{formatted_date(date)}"
+    end
+
+    def description(date)
+      "Generated from #{formatted_date(date)} BigQuery #{BIGQUERY_TABLE_ID} data"
+    end
+
+    def sample_query_set_id(date)
+      "#{BIGQUERY_TABLE_ID}_#{formatted_date(date)}"
+    end
+
+    def formatted_date(date)
+      date.strftime("%Y-%m")
+    end
+  end
+end
