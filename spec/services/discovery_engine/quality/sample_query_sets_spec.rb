c147b29a46364c74ab03d145b57d3f5e49774595
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2025-07-07 15:38:03 +0100

    Add SampleQuerySets class

---
+++ spec/services/discovery_engine/quality/sample_query_sets_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe DiscoveryEngine::Quality::SampleQuerySets do
+  subject(:sample_query_sets) { described_class.new(month_interval) }
+
+  let(:month_interval) { DiscoveryEngine::Quality::MonthInterval.new(2025, 1) }
+
+  describe "sets" do
+    it "returns SampleQuerySet objects" do
+      expect(sample_query_sets.sets.count).to eq(1)
+    end
+
+    it "creates a SampleQuerySet object for each table name" do
+      table_ids = described_class::BIGQUERY_TABLE_IDS
+      expect(DiscoveryEngine::Quality::SampleQuerySet)
+        .to receive(:new)
+        .with(month_interval, table_ids.first)
+
+      sample_query_sets.sets
+    end
+  end
+end
