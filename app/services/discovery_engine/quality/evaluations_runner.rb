763a4bb472beecb1a681b6e102d74ea34191e1c4
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2025-09-03 14:43:57 +0100

    Implement EvaluationsRunner class

--- app/services/discovery_engine/quality/evaluations_runner.rb
+++ app/services/discovery_engine/quality/evaluations_runner.rb
@@ -1,15 +1,31 @@
 module DiscoveryEngine::Quality
   class EvaluationsRunner
+    MONTH_LABELS = %i[last_month month_before_last].freeze
+
     def initialize(table_id)
       @table_id = table_id
     end
 
     def upload_detailed_metrics
-      true
+      evaluations.each do |e|
+        detailed_metrics = e.list_evaluation_results
+        Rails.logger.info detailed_metrics
+      end
     end
 
   private
 
     attr_reader :table_id
+
+    def evaluations
+      @evaluations ||=
+        sample_query_sets(table_id).map { |set| DiscoveryEngine::Quality::Evaluation.new(set) }
+    end
+
+    def sample_query_sets(table_id)
+      MONTH_LABELS.map do |month_label|
+        DiscoveryEngine::Quality::SampleQuerySet.new(table_id:, month_label:)
+      end
+    end
   end
 end
--- spec/services/discovery_engine/quality/evaluations_runner_spec.rb
+++ spec/services/discovery_engine/quality/evaluations_runner_spec.rb
@@ -1,3 +1,63 @@
 RSpec.describe DiscoveryEngine::Quality::EvaluationsRunner do
   subject(:evaluations_runner) { described_class.new("explicit") }
+
+  let(:explicit_query_set_last_month) { double("sample_query_set", table_id: "explicit", name: "/path/to/explicit-set-last_month") }
+  let(:explicit_evaluation_of_last_month) { double("evaluation", list_evaluation_results: "detailed_metrics") }
+  let(:explicit_query_set_month_before_last) { double("sample_query_set", table_id: "explicit", name: "/path/to/explicit-month_before_last") }
+  let(:explicit_evaluation_of_month_before_last) { double("evaluation", list_evaluation_results: "more_detailed_metrics") }
+
+  before do
+    allow(DiscoveryEngine::Quality::Evaluation)
+      .to receive(:new)
+      .with(explicit_query_set_last_month)
+      .and_return(explicit_evaluation_of_last_month)
+
+    allow(DiscoveryEngine::Quality::Evaluation)
+     .to receive(:new)
+     .with(explicit_query_set_month_before_last)
+     .and_return(explicit_evaluation_of_month_before_last)
+
+    { last_month: explicit_query_set_last_month,
+      month_before_last: explicit_query_set_month_before_last }.each do |label, query_set|
+      allow(DiscoveryEngine::Quality::SampleQuerySet)
+        .to receive(:new)
+        .with(table_id: "explicit", month_label: label)
+        .and_return(query_set)
+    end
+  end
+
+  describe "#upload_detailed_metrics" do
+    it "fetches explicit sample query sets for this month and the month before last" do
+      evaluations_runner.upload_detailed_metrics
+
+      expect(DiscoveryEngine::Quality::SampleQuerySet)
+        .to have_received(:new)
+        .with(table_id: "explicit", month_label: :last_month)
+      expect(DiscoveryEngine::Quality::SampleQuerySet)
+        .to have_received(:new)
+        .with(table_id: "explicit", month_label: :month_before_last)
+    end
+
+    it "creates an evaluation of each sample query set" do
+      evaluations_runner.upload_detailed_metrics
+
+      expect(DiscoveryEngine::Quality::Evaluation)
+      .to have_received(:new)
+      .with(explicit_query_set_last_month)
+
+      expect(DiscoveryEngine::Quality::Evaluation)
+       .to have_received(:new)
+       .with(explicit_query_set_month_before_last)
+    end
+
+    it "sends .list_evaluation_results to each evaluation" do
+      evaluations_runner.upload_detailed_metrics
+
+      expect(explicit_evaluation_of_last_month)
+        .to have_received(:list_evaluation_results)
+
+      expect(explicit_evaluation_of_month_before_last)
+        .to have_received(:list_evaluation_results)
+    end
+  end
 end
