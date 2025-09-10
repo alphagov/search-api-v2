9c54b27d889f3aaf34360a92cd04b343c28c972c
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2025-09-10 16:15:06 +0100

    Create shared context and examples before extending tests

--- spec/services/discovery_engine/quality/evaluations_runner_spec.rb
+++ spec/services/discovery_engine/quality/evaluations_runner_spec.rb
@@ -1,76 +1,90 @@
-RSpec.describe DiscoveryEngine::Quality::EvaluationsRunner do
-  subject(:evaluations_runner) { described_class.new("explicit") }
-
+RSpec.shared_context "with a table_id" do
   let(:last_month_partition_date) { Date.new(1979, 10, 1) }
   let(:month_before_last_partition_date) { Date.new(1979, 9, 1) }
-  let(:explicit_query_set_last_month) { double("sample_query_set", table_id: "explicit", name: "/path/to/explicit-set-last_month") }
-  let(:explicit_evaluation_of_last_month) { double("evaluation", list_evaluation_results: "detailed_metrics", formatted_create_time: "time-stamp", partition_date: last_month_partition_date) }
-  let(:explicit_query_set_month_before_last) { double("sample_query_set", table_id: "explicit", name: "/path/to/explicit-month_before_last") }
-  let(:explicit_evaluation_of_month_before_last) { double("evaluation", list_evaluation_results: "more_detailed_metrics", formatted_create_time: "time-stamp", partition_date: month_before_last_partition_date) }
-  let(:gcp_bucket_exporter) { double("gcp_bucket_exporter") }
+  let(:query_set_last_month) { double("sample_query_set", table_id:, name: "/path/to/#{table_id}-set-last_month") }
+  let(:evaluation_of_last_month) { double("evaluation", list_evaluation_results: "detailed_metrics", formatted_create_time: "time-stamp", partition_date: last_month_partition_date) }
+  let(:query_set_month_before_last) { double("sample_query_set", table_id:, name: "/path/to/#{table_id}-month_before_last") }
+  let(:evaluation_of_month_before_last) { double("evaluation", list_evaluation_results: "more_detailed_metrics", formatted_create_time: "time-stamp", partition_date: month_before_last_partition_date) }
 
   before do
     allow(DiscoveryEngine::Quality::Evaluation)
       .to receive(:new)
-      .with(explicit_query_set_last_month)
-      .and_return(explicit_evaluation_of_last_month)
+      .with(query_set_last_month)
+      .and_return(evaluation_of_last_month)
 
     allow(DiscoveryEngine::Quality::Evaluation)
      .to receive(:new)
-     .with(explicit_query_set_month_before_last)
-     .and_return(explicit_evaluation_of_month_before_last)
+     .with(query_set_month_before_last)
+     .and_return(evaluation_of_month_before_last)
 
-    { last_month: explicit_query_set_last_month,
-      month_before_last: explicit_query_set_month_before_last }.each do |label, query_set|
+    { last_month: query_set_last_month,
+      month_before_last: query_set_month_before_last }.each do |label, query_set|
       allow(DiscoveryEngine::Quality::SampleQuerySet)
         .to receive(:new)
-        .with(table_id: "explicit", month_label: label)
+        .with(table_id:, month_label: label)
         .and_return(query_set)
     end
+  end
+end
 
-    allow(DiscoveryEngine::Quality::GcpBucketExporter)
-      .to receive(:new)
-      .and_return(gcp_bucket_exporter)
-
-    allow(gcp_bucket_exporter)
-      .to receive(:send)
-      .with(anything, anything, anything, anything)
-      .and_return(true)
+RSpec.shared_examples "creates sample query sets and evaluations" do |table_id|
+  before do
+    evaluations_runner.upload_detailed_metrics
   end
 
-  describe "#upload_detailed_metrics" do
-    it "fetches explicit sample query sets for this month and the month before last" do
-      evaluations_runner.upload_detailed_metrics
-
-      expect(DiscoveryEngine::Quality::SampleQuerySet)
-        .to have_received(:new)
-        .with(table_id: "explicit", month_label: :last_month)
-      expect(DiscoveryEngine::Quality::SampleQuerySet)
-        .to have_received(:new)
-        .with(table_id: "explicit", month_label: :month_before_last)
-    end
-
-    it "creates an evaluation of each sample query set" do
-      evaluations_runner.upload_detailed_metrics
-
-      expect(DiscoveryEngine::Quality::Evaluation)
+  it "fetches explicit sample query sets for this month and the month before last" do
+    expect(DiscoveryEngine::Quality::SampleQuerySet)
       .to have_received(:new)
-      .with(explicit_query_set_last_month)
+      .with(table_id:, month_label: :last_month)
+    expect(DiscoveryEngine::Quality::SampleQuerySet)
+      .to have_received(:new)
+      .with(table_id:, month_label: :month_before_last)
+  end
 
-      expect(DiscoveryEngine::Quality::Evaluation)
-       .to have_received(:new)
-       .with(explicit_query_set_month_before_last)
-    end
+  it "creates an evaluation of each sample query set" do
+    expect(DiscoveryEngine::Quality::Evaluation)
+    .to have_received(:new)
+    .with(query_set_last_month)
 
-    it "sends list_evaluation_results for each evaluation to a gcp bucket" do
-      evaluations_runner.upload_detailed_metrics
+    expect(DiscoveryEngine::Quality::Evaluation)
+      .to have_received(:new)
+      .with(query_set_month_before_last)
+  end
+end
 
-      evaluations = [explicit_evaluation_of_last_month, explicit_evaluation_of_month_before_last]
-      expect(evaluations).to all(have_received(:list_evaluation_results))
-      expect(evaluations).to all(have_received(:formatted_create_time))
+RSpec.describe DiscoveryEngine::Quality::EvaluationsRunner do
+  subject(:evaluations_runner) { described_class.new(table_id) }
 
-      expect(gcp_bucket_exporter).to have_received(:send).with("time-stamp", "explicit", last_month_partition_date, "detailed_metrics").once
-      expect(gcp_bucket_exporter).to have_received(:send).with("time-stamp", "explicit", month_before_last_partition_date, "more_detailed_metrics").once
+  context "when a table id is passed in" do
+    include_context "with a table_id"
+
+    let(:table_id) { "explicit" }
+    let(:gcp_bucket_exporter) { double("gcp_bucket_exporter") }
+
+    describe "#upload_detailed_metrics" do
+      before do
+        allow(DiscoveryEngine::Quality::GcpBucketExporter)
+          .to receive(:new)
+          .and_return(gcp_bucket_exporter)
+
+        allow(gcp_bucket_exporter)
+          .to receive(:send)
+          .with(anything, anything, anything, anything)
+          .and_return(true)
+      end
+
+      it_behaves_like "creates sample query sets and evaluations", "explicit"
+
+      it "sends list_evaluation_results for each evaluation to a gcp bucket" do
+        evaluations_runner.upload_detailed_metrics
+
+        evaluations = [evaluation_of_last_month, evaluation_of_month_before_last]
+        expect(evaluations).to all(have_received(:list_evaluation_results))
+        expect(evaluations).to all(have_received(:formatted_create_time))
+
+        expect(gcp_bucket_exporter).to have_received(:send).with("time-stamp", "explicit", last_month_partition_date, "detailed_metrics").once
+        expect(gcp_bucket_exporter).to have_received(:send).with("time-stamp", "explicit", month_before_last_partition_date, "more_detailed_metrics").once
+      end
     end
   end
 end
