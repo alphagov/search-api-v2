aad0a34069ba3c988aafd3b715c41fa85ab81b7a
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2025-09-16 08:43:39 +0100

    Add quality metrics to the Prometheus registry

--- app/services/discovery_engine/quality/prometheus_reporter.rb
+++ app/services/discovery_engine/quality/prometheus_reporter.rb
@@ -1,8 +1,21 @@
 module DiscoveryEngine::Quality
   class PrometheusReporter
     def send(quality_metrics, evaluation)
-      Rails.logger.info("#{quality_metrics}, #{evaluation}")
-      true
+      metric_collector.record_evaluations(
+        quality_metrics,
+        evaluation.month_label,
+        evaluation.table_id,
+      )
+    end
+
+  private
+
+    def metric_collector
+      @metric_collector ||= Metrics::Evaluation.new(registry)
+    end
+
+    def registry
+      @registry ||= Prometheus::Client.registry
     end
   end
 end
--- spec/services/discovery_engine/quality/prometheus_reporter_spec.rb
+++ spec/services/discovery_engine/quality/prometheus_reporter_spec.rb
@@ -1,12 +1,33 @@
 RSpec.describe DiscoveryEngine::Quality::PrometheusReporter do
   subject(:prometheus_reporter) { described_class.new }
 
-  let(:evaluation) { double("evaluation") }
+  let(:evaluation) { double("evaluation", quality_metrics:, month_label: :last_month, table_id: "explicit") }
   let(:quality_metrics) { "quality_metrics" }
+  let(:registry) { double("registry", gauge: nil) }
+  let(:metric_collector) { double("metric_collector") }
+
+  before do
+    allow(Prometheus::Client)
+      .to receive(:registry)
+      .and_return(registry)
+
+    allow(Metrics::Evaluation)
+      .to receive(:new)
+      .with(registry)
+      .and_return(metric_collector)
+
+    allow(metric_collector)
+      .to receive(:record_evaluations)
+      .with(anything, anything, anything)
+  end
 
   describe "send" do
-    it "is to be implemented" do
-      expect(prometheus_reporter.send(quality_metrics, evaluation)).to be_truthy
+    it "adds metrics to the Prometheus registry" do
+      prometheus_reporter.send(quality_metrics, evaluation)
+
+      expect(metric_collector)
+        .to have_received(:record_evaluations)
+        .with(quality_metrics, :last_month, "explicit")
     end
   end
 end
