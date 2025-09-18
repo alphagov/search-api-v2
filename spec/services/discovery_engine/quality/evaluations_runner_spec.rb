908e6166ad58b25cab4505ae3256b98e68322743
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2025-09-18 17:56:47 +0100

    Replace doubles with verifying doubles

--- spec/services/discovery_engine/quality/evaluations_runner_spec.rb
+++ spec/services/discovery_engine/quality/evaluations_runner_spec.rb
@@ -1,27 +1,28 @@
 RSpec.describe DiscoveryEngine::Quality::EvaluationsRunner do
   subject(:evaluations_runner) { described_class.new("explicit") }
 
+  let(:table_id) { "explicit" }
   let(:last_month_partition_date) { Date.new(1979, 10, 1) }
   let(:month_before_last_partition_date) { Date.new(1979, 9, 1) }
-  let(:explicit_query_set_last_month) { double("sample_query_set", table_id: "explicit", name: "/path/to/explicit-set-last_month") }
-  let(:explicit_evaluation_of_last_month) { double("evaluation", list_evaluation_results: "detailed_metrics", formatted_create_time: "time-stamp", partition_date: last_month_partition_date) }
-  let(:explicit_query_set_month_before_last) { double("sample_query_set", table_id: "explicit", name: "/path/to/explicit-month_before_last") }
-  let(:explicit_evaluation_of_month_before_last) { double("evaluation", list_evaluation_results: "more_detailed_metrics", formatted_create_time: "time-stamp", partition_date: month_before_last_partition_date) }
-  let(:gcp_bucket_exporter) { double("gcp_bucket_exporter") }
+  let(:query_set_last_month) { instance_double(DiscoveryEngine::Quality::SampleQuerySet, table_id:, name: "/path/to/#{table_id}-set-last_month") }
+  let(:evaluation_of_last_month) { instance_double(DiscoveryEngine::Quality::Evaluation, list_evaluation_results: "detailed_metrics", formatted_create_time: "time-stamp", partition_date: last_month_partition_date) }
+  let(:query_set_month_before_last) { instance_double(DiscoveryEngine::Quality::SampleQuerySet, table_id:, name: "/path/to/#{table_id}-month_before_last") }
+  let(:evaluation_of_month_before_last) { instance_double(DiscoveryEngine::Quality::Evaluation, list_evaluation_results: "more_detailed_metrics", formatted_create_time: "time-stamp", partition_date: month_before_last_partition_date) }
+  let(:gcp_bucket_exporter) { instance_double(DiscoveryEngine::Quality::GcpBucketExporter) }
 
   before do
     allow(DiscoveryEngine::Quality::Evaluation)
       .to receive(:new)
-      .with(explicit_query_set_last_month)
-      .and_return(explicit_evaluation_of_last_month)
+      .with(query_set_last_month)
+      .and_return(evaluation_of_last_month)
 
     allow(DiscoveryEngine::Quality::Evaluation)
      .to receive(:new)
-     .with(explicit_query_set_month_before_last)
-     .and_return(explicit_evaluation_of_month_before_last)
+     .with(query_set_month_before_last)
+     .and_return(evaluation_of_month_before_last)
 
-    { last_month: explicit_query_set_last_month,
-      month_before_last: explicit_query_set_month_before_last }.each do |label, query_set|
+    { last_month: query_set_last_month,
+      month_before_last: query_set_month_before_last }.each do |label, query_set|
       allow(DiscoveryEngine::Quality::SampleQuerySet)
         .to receive(:new)
         .with(table_id: "explicit", month_label: label)
@@ -44,10 +45,10 @@ RSpec.describe DiscoveryEngine::Quality::EvaluationsRunner do
 
       expect(DiscoveryEngine::Quality::SampleQuerySet)
         .to have_received(:new)
-        .with(table_id: "explicit", month_label: :last_month)
+        .with(table_id:, month_label: :last_month)
       expect(DiscoveryEngine::Quality::SampleQuerySet)
         .to have_received(:new)
-        .with(table_id: "explicit", month_label: :month_before_last)
+        .with(table_id:, month_label: :month_before_last)
     end
 
     it "creates an evaluation of each sample query set" do
@@ -55,17 +56,17 @@ RSpec.describe DiscoveryEngine::Quality::EvaluationsRunner do
 
       expect(DiscoveryEngine::Quality::Evaluation)
       .to have_received(:new)
-      .with(explicit_query_set_last_month)
+      .with(query_set_last_month)
 
       expect(DiscoveryEngine::Quality::Evaluation)
        .to have_received(:new)
-       .with(explicit_query_set_month_before_last)
+       .with(query_set_month_before_last)
     end
 
     it "sends list_evaluation_results for each evaluation to a gcp bucket" do
       evaluations_runner.upload_detailed_metrics
 
-      evaluations = [explicit_evaluation_of_last_month, explicit_evaluation_of_month_before_last]
+      evaluations = [evaluation_of_last_month, evaluation_of_month_before_last]
       expect(evaluations).to all(have_received(:list_evaluation_results))
       expect(evaluations).to all(have_received(:formatted_create_time))
 
