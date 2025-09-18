10686551cdc7697a54649b24cc86923553883beb
Jos Koetsier <jos.koetsier@digital.cabinet-office.gov.uk>
2025-09-18 13:18:01 +0100

    Handle invalid query parameters
    Ensure that searching with a 'q' parameter that is not a String
    (e.g. an Array or a Hash) is handled gracefully. Previously, this
    case was unhandled and caused errors to be reported in Sentry

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,6 @@
 class SearchesController < ApplicationController
+  rescue_from ArgumentError, with: :render_argument_error
+
   def show
     render json: DiscoveryEngine::Query::Search.new(query_params, user_agent: request.user_agent).result_set
   end
@@ -7,5 +9,9 @@ private
 
   def query_params
     params.permit!
+  end
+
+  def render_argument_error(exception)
+    render json: { "error": exception.message }, status: :unprocessable_content
   end
 end
