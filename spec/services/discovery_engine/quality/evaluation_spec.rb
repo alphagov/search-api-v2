5dcef565e4ec3dbe9c35dfadbf9cab330a6ef8e4
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2025-10-13 20:00:24 +0100

    Reorganise tests

--- spec/services/discovery_engine/quality/evaluation_spec.rb
+++ spec/services/discovery_engine/quality/evaluation_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe DiscoveryEngine::Quality::Evaluation do
                     partition_date: date)
   end
   let(:evaluation) { described_class.new(sample_set) }
-  let(:evaluation_service) { double("evaluation_service", create_evaluation: operation, get_evaluation: new_evaluation, list_evaluations: [new_evaluation]) }
+  let(:evaluation_service) { double("evaluation_service", create_evaluation: operation, get_evaluation: new_evaluation) }
   let(:operation) { double("operation", error?: false, wait_until_done!: true, results: operation_results) }
   let(:operation_results) { double("operation_results", name: new_evaluation_name) }
   let(:new_evaluation_name) { "/evaluations/1" }
@@ -30,7 +30,6 @@ RSpec.describe DiscoveryEngine::Quality::Evaluation do
            create_time: google_time_stamp,
            evaluation_spec: evaluation_spec)
   end
-  let(:evaluation_pending) { double("evaluation", state: :PENDING, name: "/evaluations/2") }
 
   let(:quality_metrics_response) do
     {
@@ -70,121 +69,118 @@ RSpec.describe DiscoveryEngine::Quality::Evaluation do
       it_behaves_like "waits for running evaluations to complete", :PENDING, :RUNNING, :SUCCEEDED
     end
 
-    it "sends a create evaluation request to the evaluations service" do
-      evaluation.quality_metrics
+    context "when the evaluations service is not busy" do
+      let(:complete_evaluation) { double("evaluation", state: :SUCCEEDED) }
 
-      expect(evaluation_service)
-        .to have_received(:create_evaluation)
-        .with(
-          parent: Rails.application.config.discovery_engine_default_location_name,
-          evaluation: {
-            evaluation_spec: {
-              query_set_spec: {
-                sample_query_set: sample_set.name,
-              },
-              search_request: {
-                serving_config: ServingConfig.default.name,
+      before do
+        allow(evaluation_service).to receive(:list_evaluations).and_return([complete_evaluation])
+      end
+
+      it "sends a create evaluation request to the evaluations service" do
+        evaluation.quality_metrics
+
+        expect(evaluation_service)
+          .to have_received(:create_evaluation)
+          .with(
+            parent: Rails.application.config.discovery_engine_default_location_name,
+            evaluation: {
+              evaluation_spec: {
+                query_set_spec: {
+                  sample_query_set: sample_set.name,
+                },
+                search_request: {
+                  serving_config: ServingConfig.default.name,
+                },
               },
             },
-          },
-        )
-    end
-
-    context "when the create evaluations request does not complete" do
-      let(:error) { double("error", message: "An error message") }
-      let(:operation) { double("operation", wait_until_done!: true, error?: true, error:) }
-
-      it "raises an error" do
-        expect { evaluation.quality_metrics }.to raise_error("An error message")
-      end
-    end
-
-    context "when GCP returns an AlreadyExistsError" do
-      let(:erroring_evaluation_service) { double("erroring_evaluation_service") }
-
-      before do
-        allow(DiscoveryEngine::Clients).to receive(:evaluation_service).and_return(erroring_evaluation_service)
-
-        allow(erroring_evaluation_service)
-          .to receive(:list_evaluations)
-          .with(parent: Rails.application.config.discovery_engine_default_location_name)
-          .and_return([new_evaluation])
-
-        allow(erroring_evaluation_service)
-          .to receive(:create_evaluation)
-          .with(anything)
-          .and_raise(Google::Cloud::AlreadyExistsError)
-
-        allow(GovukError).to receive(:notify)
+          )
       end
 
-      it "logs then raises the error" do
-        expect { evaluation.quality_metrics }.to raise_error(Google::Cloud::AlreadyExistsError)
+      context "when the create evaluations request does not complete" do
+        let(:error) { double("error", message: "An error message") }
+        let(:operation) { double("operation", wait_until_done!: true, error?: true, error:) }
 
-        expect(GovukError).to have_received(:notify)
-          .with("No evaluation created of sample set clickstream 2025-10 (Google::Cloud::AlreadyExistsError)")
-      end
-    end
-
-    context "when the evaluation completes and has a state of :SUCCEEDED" do
-      it "fetches the evaluation" do
-        evaluation.quality_metrics
-
-        expect(evaluation_service).to have_received(:get_evaluation)
-          .with(name: new_evaluation.name)
-
-        expect(Rails.logger).to have_received(:info)
-          .with("Successfully created an evaluation of sample set clickstream 2025-10")
+        it "raises an error" do
+          expect { evaluation.quality_metrics }.to raise_error("An error message")
+        end
       end
 
-      it "fetches quality metrics" do
-        evaluation.quality_metrics
+      context "when GCP returns an AlreadyExistsError" do
+        before do
+          allow(evaluation_service)
+            .to receive(:create_evaluation)
+            .with(anything)
+            .and_raise(Google::Cloud::AlreadyExistsError)
 
-        expect(new_evaluation)
-          .to have_received(:quality_metrics)
+          allow(GovukError).to receive(:notify)
+        end
+
+        it "logs then raises the error" do
+          expect { evaluation.quality_metrics }.to raise_error(Google::Cloud::AlreadyExistsError)
+
+          expect(GovukError).to have_received(:notify)
+            .with("No evaluation created of sample set clickstream 2025-10 (Google::Cloud::AlreadyExistsError)")
+        end
+      end
+
+      context "when the evaluation completes and has a state of :SUCCEEDED" do
+        it "fetches the evaluation" do
+          evaluation.quality_metrics
+
+          expect(evaluation_service).to have_received(:get_evaluation)
+            .with(name: new_evaluation.name)
+
+          expect(Rails.logger).to have_received(:info)
+            .with("Successfully created an evaluation of sample set #{sample_set.display_name}")
+        end
+
+        it "fetches quality metrics" do
+          evaluation.quality_metrics
+
+          expect(new_evaluation)
+            .to have_received(:quality_metrics)
+            .once
+        end
+
+        it "calls quality_metrics on the memoised evaluation object" do
+          evaluation.quality_metrics
+          evaluation.quality_metrics
+
+          expect(evaluation_service).to have_received(:create_evaluation)
           .once
+
+          expect(evaluation_service).to have_received(:get_evaluation)
+            .with(name: new_evaluation.name)
+            .once
+        end
       end
 
-      it "calls quality_metrics on the memoised evaluation object" do
-        evaluation.quality_metrics
-        evaluation.quality_metrics
+      context "when the evaluation is still being processed and has a state of :PENDING" do
+        before do
+          allow(new_evaluation).to receive(:state).and_return(:PENDING, :SUCCEEDED)
+        end
 
-        expect(evaluation_service).to have_received(:create_evaluation)
-         .once
+        it "sleeps for 10, then polls again" do
+          evaluation.quality_metrics
 
-        expect(evaluation_service).to have_received(:get_evaluation)
-          .with(name: new_evaluation.name)
-          .once
-      end
-    end
+          expect(evaluation_service).to have_received(:get_evaluation)
+            .with(name: new_evaluation.name)
+            .twice
 
-    context "when the evaluation is still being processed and has a state of :PENDING" do
-      let(:evaluation_pending) { double("evaluation", state: :PENDING) }
+          expect(Kernel).to have_received(:sleep)
 
-      before do
-        allow(evaluation_service).to receive(:get_evaluation)
-          .with(name: new_evaluation.name)
-          .and_return(evaluation_pending, new_evaluation)
-      end
-
-      it "sleeps for 10, then polls again" do
-        evaluation.quality_metrics
-
-        expect(evaluation_service).to have_received(:get_evaluation)
-          .with(name: new_evaluation.name)
-          .twice
-
-        expect(Kernel).to have_received(:sleep)
-
-        expect(Rails.logger).to have_received(:info).with("Still waiting for evaluation to complete...")
+          expect(Rails.logger).to have_received(:info).with("Still waiting for evaluation to complete...")
+        end
       end
     end
   end
 
   describe "#list_evaluation_results" do
     let(:list_evaluation_results) { double("list_evaluation_results") }
+    let(:complete_evaluation) { double("evaluation", state: :SUCCEEDED) }
 
     before do
+      allow(evaluation_service).to receive(:list_evaluations).and_return([complete_evaluation])
       allow(DiscoveryEngine::Quality::ListEvaluationResults)
         .to receive(:new)
         .with(anything, anything, anything)
@@ -216,11 +212,9 @@ RSpec.describe DiscoveryEngine::Quality::Evaluation do
   end
 
   describe "#formatted_create_time" do
-    before do
-      allow(evaluation_service).to receive(:get_evaluation)
-        .with(name: new_evaluation.name)
-        .and_return(new_evaluation)
-    end
+    let(:complete_evaluation) { double("evaluation", state: :SUCCEEDED) }
+
+    before { allow(evaluation_service).to receive(:list_evaluations).and_return([complete_evaluation]) }
 
     it "formats the create_time stamp from the google response" do
       evaluation.quality_metrics
