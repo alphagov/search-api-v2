bb996244f2405ee8f95af826b13b2fcf9b3d910e
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2025-10-28 13:59:52 +0000

    Remove v1beta client methods
    The evaluations rely on features in the
    google-cloud-discovery_engine-v1beta gem. This gem is incompatible with
    the main client (google-cloud-discovery_engine) gem, so the evaluations
    code has been moved to it's [own app](https://github.com/alphagov/search-api-v2-beta-features).
    
    `cloud_storage_service` has also been removed as this was only used to
    send evaluation metrics to a GCP bucket.

--- app/services/discovery_engine/clients.rb
+++ app/services/discovery_engine/clients.rb
@@ -1,13 +1,6 @@
-require "google/cloud/discovery_engine/v1beta"
-require "google/cloud/storage"
-
 module DiscoveryEngine
   module Clients
     extend self
-
-    def cloud_storage_service
-      @cloud_storage_service ||= Google::Cloud::Storage
-    end
 
     def completion_service
       @completion_service ||= Google::Cloud::DiscoveryEngine.completion_service(version: :v1)
@@ -17,30 +10,12 @@ module DiscoveryEngine
       @document_service ||= Google::Cloud::DiscoveryEngine.document_service(version: :v1)
     end
 
-    def evaluation_service
-      @evaluation_service ||= v1beta_api::EvaluationService::Client.new
-    end
-
-    def sample_query_service
-      @sample_query_service ||= v1beta_api::SampleQueryService::Client.new
-    end
-
-    def sample_query_set_service
-      @sample_query_set_service ||= v1beta_api::SampleQuerySetService::Client.new
-    end
-
     def search_service
       @search_service ||= Google::Cloud::DiscoveryEngine.search_service(version: :v1)
     end
 
     def user_event_service
       @user_event_service ||= Google::Cloud::DiscoveryEngine.user_event_service(version: :v1)
-    end
-
-  private
-
-    def v1beta_api
-      Google::Cloud::DiscoveryEngine::V1beta
     end
   end
 end
