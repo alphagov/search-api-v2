6daecc0731d3ded32db14a31e1495818e760a37b
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2026-03-31 20:39:59 +0100

    Apply timeout on critical Google client calls
    The completion and search APIs are our most time-critical interactions.
    
    I don't believe there's a default timeout which leaves us vulnerable to resource exhaustion if we experience slowness in the Google backend, as the rails threads will continue to wait, and we'll gradually run out.
    
    The gds-api-adapters call from finder-frontend times out after 4 seconds, so it doesn't make sense for us to continue processing searches that take longer than this.
    
    We could tweak the gds-api-adapters config for searches if we were confident in our sums, but I've set autocomplete as 1s and search as 4s. I don't believe users will mind too much if the odd autocomplete call times out, whereas it's much more irritating for a search to timeout prematurely.
    
    An enhancement which has been suggested, given that the mean search time is much lower than 4s, is to set a short timeout on the initial call and use progressive backoffs. If the initial search hasn't completed in 1s, it may not be likely to work at all. It's possible to set a [retry policy](https://docs.cloud.google.com/ruby/docs/reference/google-cloud-discovery_engine-v1/latest/Google-Cloud-DiscoveryEngine-V1-DataStoreService-Client-Configuration#Google__Cloud__DiscoveryEngine__V1__DataStoreService__Client__Configuration_retry_policy_instance_) in the client configuration, or we could write our own handler if we wanted.

---
+++ spec/services/discovery_engine/clients_spec.rb
@@ -0,0 +1,41 @@
+RSpec.describe DiscoveryEngine::Clients do
+  after do
+    described_class.instance_variable_set(:@completion_service, nil)
+    described_class.instance_variable_set(:@search_service, nil)
+  end
+
+  shared_examples "a client with timeout configured" do |service_name, timeout|
+    let(:config) { double("config") }
+    let(:client) { double("client") }
+
+    before do
+      allow(config).to receive(:timeout=)
+      allow(Google::Cloud::DiscoveryEngine).to receive(service_name) do |**_kwargs, &block|
+        block.call(config) if block
+        client
+      end
+    end
+
+    it "initialises the client with the v1 API version" do
+      subject
+      expect(Google::Cloud::DiscoveryEngine).to have_received(service_name).with(version: :v1)
+    end
+
+    it "configures the client with a timeout" do
+      subject
+      expect(config).to have_received(:timeout=).with(timeout)
+    end
+  end
+
+  describe ".search_service with 3 second timeout" do
+    subject { described_class.search_service }
+
+    include_examples "a client with timeout configured", :search_service, 4.seconds
+  end
+
+  describe ".completion_service with one second timeout" do
+    subject { described_class.completion_service }
+
+    include_examples "a client with timeout configured", :completion_service, 1.second
+  end
+end
