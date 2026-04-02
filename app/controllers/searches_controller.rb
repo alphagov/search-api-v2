e2fdab52cf0e9a4870578392a9e67de8250046c2
Jessica Jones <jessica.jones@digital.cabinet-office.gov.uk>
2026-04-02 20:32:45 +0100

    Add the vertex response time to the response header
    This is a bit of a workaround so that we can see vertex response
    time for individual queries in the kibana logs.
    
    However it looks like there is still an issue with the logstasher
    initialisation - running the app in docker I can see errors that
    were supposed to have been fixed by
    
    https://github.com/alphagov/govuk_app_config/pull/561/changes
    https://github.com/alphagov/govuk_app_config/pull/564/changes

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,12 @@
 class SearchesController < ApplicationController
   before_action :validate_query_params, only: :show
+  after_action :add_response_time_header
 
   def show
-    ActiveSupport::Notifications.instrument("vertex_search_request_duration") do
-      render json: DiscoveryEngine::Query::Search.new(query_params, user_agent: request.user_agent).result_set
+    with_duration_logging do
+      @search_results = DiscoveryEngine::Query::Search.new(query_params, user_agent: request.user_agent).result_set
     end
+    render json: @search_results
   end
 
 private
@@ -15,5 +17,15 @@ private
 
   def validate_query_params
     raise ActionController::BadRequest, "Invalid query parameter" unless params.fetch(:q, "").is_a?(String)
+  end
+
+  def with_duration_logging(&block)
+    @duration = Benchmark.realtime(&block)
+  end
+
+  def add_response_time_header
+    return unless @duration
+
+    response.headers["Vertex-Response-Time"] = @duration.round(4).to_s
   end
 end
--- config/initializers/logstasher.rb
+++ config/initializers/logstasher.rb
@@ -1,11 +1,7 @@
 if ENV["GOVUK_RAILS_JSON_LOGGING"].present?
-  ActiveSupport::Notifications.subscribe("vertex_search_request_duration") do |event|
-    duration_in_seconds = event.duration / 1000.0
-
-    GovukJsonLogging.configure do
-      add_custom_fields do |fields|
-        fields[:vertex_search_duration] = duration_in_seconds
-      end
+  GovukJsonLogging.configure do
+    add_custom_fields do |fields|
+      fields[:vertex_search_duration] = response.headers["Vertex-Response-Time"]
     end
   end
 end
